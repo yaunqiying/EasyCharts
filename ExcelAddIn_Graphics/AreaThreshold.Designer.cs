c13928428ea60a7d42aa9222715a7e90db84b246
EasyChart <easycharts@hotmail.com>
2019-05-30 15:25:18 +0800

    Add files via upload

---
+++ ExcelAddIn_Graphics/AreaThreshold.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace ExcelAddIn_Graphics
+{
+    partial class AreaThreshold
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(AreaThreshold));
+            this.label1 = new System.Windows.Forms.Label();
+            this.button_OK = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.textBox_Bandwidth = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.SkyBlue;
+            this.label1.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label1.Location = new System.Drawing.Point(33, 340);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(100, 21);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "Y轴阈值参数";
+//            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // button_OK
+            // 
+            this.button_OK.Location = new System.Drawing.Point(255, 338);
+            this.button_OK.Name = "button_OK";
+            this.button_OK.Size = new System.Drawing.Size(81, 26);
+            this.button_OK.TabIndex = 9;
+            this.button_OK.Text = "确定";
+            this.button_OK.UseVisualStyleBackColor = true;
+            this.button_OK.Click += new System.EventHandler(this.button_OK_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(31, 17);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(305, 301);
+            this.pictureBox1.TabIndex = 8;
+            this.pictureBox1.TabStop = false;
+        //    this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // textBox_Bandwidth
+            // 
+            this.textBox_Bandwidth.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.textBox_Bandwidth.Location = new System.Drawing.Point(139, 338);
+            this.textBox_Bandwidth.Name = "textBox_Bandwidth";
+            this.textBox_Bandwidth.Size = new System.Drawing.Size(73, 29);
+            this.textBox_Bandwidth.TabIndex = 11;
+            this.textBox_Bandwidth.Text = "0.0";
+            this.textBox_Bandwidth.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textBox_Bandwidth.TextChanged += new System.EventHandler(this.textBox_Bandwidth_TextChanged);
+            // 
+            // AreaThreshold
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(363, 380);
+            this.Controls.Add(this.textBox_Bandwidth);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button_OK);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "AreaThreshold";
+            this.Text = "AreaThreshold";
+          //  this.Load += new System.EventHandler(this.AreaThreshold_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button_OK;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox textBox_Bandwidth;
+    }
+}
